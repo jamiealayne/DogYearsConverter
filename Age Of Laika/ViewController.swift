f6cec68245685a45ce9416bc631e1f9f121ae57b
Jamie Layne <jamielayne@Jamies-MacBook-Pro.local>
2014-09-16 10:05:26 -0400

    First Challenge Submission

--- Age Of Laika/ViewController.swift
+++ Age Of Laika/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet var humanYearsField: UITextField!
+	@IBOutlet var dogYearsLabel: UILabel!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,14 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	@IBAction func convertToDogYears(sender: AnyObject) {
+		
+		let humanYears = humanYearsField.text.toInt()!
+		let dogYears = humanYears * 7
+		
+		dogYearsLabel.hidden = false
+		dogYearsLabel.text = "\(humanYears) human years is \(dogYears) dog years"
+	}
 
 }
 
